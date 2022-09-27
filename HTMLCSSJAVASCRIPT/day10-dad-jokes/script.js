449bab67062baf8b714ac1d10dac0406fab26ea5
Diego Neves <pdcassiano@gmail.com>
2022-09-27 12:12:33 -0300

    conclusão day 10

--- HTMLCSSJAVASCRIPT/day10-dad-jokes/script.js
+++ HTMLCSSJAVASCRIPT/day10-dad-jokes/script.js
@@ -5,14 +5,24 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: { Accept: 'application/json' },
   }
 
-  fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke
-    })
+  const res = await fetch('https://icanhazdadjoke.com', config)
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
+// function generateJoke() {
+//   const config = {
+//     headers: { Accept: 'application/json' },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
